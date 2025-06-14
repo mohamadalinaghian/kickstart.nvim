0a12e0588c9d7c729c8ce0ed4ce02e453d2a8642
mohamad <mohamadalinaghian@gmail.com>
2025-06-14 22:08:44 +0330

    move all plugins to lua dir and try to create new lsp system

---
+++ lua/options.lua
@@ -0,0 +1,46 @@
+vim.o.splitbelow = false
+vim.o.splitright = false
+
+vim.g.have_nerd_font = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.mouse = 'nv'
+
+vim.o.showmode = false
+
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+vim.o.breakindent = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.signcolumn = 'yes'
+
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.o.inccommand = 'split'
+
+vim.o.cursorline = true
+
+vim.o.scrolloff = 25
+
+vim.o.confirm = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,6 +6,20 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
+
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 800,
+        virt_text_pos = 'eol',
+      },
+
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
